4d3816c9e0eaf02857bb9c4403146d6cad962375
scripter-kartik <agarwalkartik704@gmail.com>
2026-01-04 17:57:27 +0530

    add server.js file

---
+++ EasyExplain-backend/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import OpenAI from "openai"
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
